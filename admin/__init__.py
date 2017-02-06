1a19d78851e7965de2c7212a3b1ecd922a838827
tikismoke <webmaster@tikijs.dyndns.org>
2017-02-06 20:38:32 +0100

    Add token generation, device creation #6, device detection from admin #4

--- admin/__init__.py
+++ admin/__init__.py
@@ -16,9 +16,33 @@ import nest
 import locale
 import time
 import json
+import os
+from flask_wtf import Form
+from wtforms import StringField
+from flask import request, flash
+try:
+    from flask.ext.babel import gettext, ngettext
+except ImportError:
+    from flask_babel import gettext, ngettext
 
+    pass
 
 ### package specific functions
+
+def get_token_link(product_id , product_secret, access_token_cache_file):
+    napi = nest.Nest(client_id=product_id, client_secret=product_secret, access_token_cache_file=access_token_cache_file)
+    login_url = napi.authorize_url
+    login_url = unicode(login_url, 'utf-8')
+    return login_url
+
+def generate_token_file(authorization_code, product_id , product_secret, access_token_cache_file):
+    napi = nest.Nest(client_id=product_id, client_secret=product_secret, access_token_cache_file=access_token_cache_file)
+    napi.request_token(authorization_code)
+    if napi.invalid_access_token is True:
+        flash(gettext(u"Error while getting token from Nest code, check you client id/secret redirect url or code"),
+              "error")
+    else:
+        flash(gettext(u"Successfully generate token. Please restart the plugin."), "success")
 
 def get_info_from_log(cmd):
     print("Command = %s" % cmd)
@@ -30,31 +54,38 @@ def get_info_from_log(cmd):
 
 def get_device_list(product_id , product_secret, access_token_cache_file):
     napi = nest.Nest(client_id=product_id, client_secret=product_secret, access_token_cache_file=access_token_cache_file)
-    try:
-	return_value = ""
-	for structure in napi.structures:
-	    return_value =  return_value + "Nest.Home name: " + " " + str(structure.name) + "\n"
-            for ProtectDevice in structure.protectdevices:
-		return_value = return_value + "Nest.Protect serial: " + " " + str(ProtectDevice.where) + " " + str(ProtectDevice.serial) + "\n"
-            for device in structure.devices:
-	        return_value = return_value + "Nest.Thermostat serial: " + " " + str(device.where) + " " + str(device.name) + "\n"
-        return return_value
-    except:
-	return unicode("ERROR getting Nest information!\nCheck your configuration.\nOr wait some time if you do too much request in little time.", 'utf-8')
+#    try:
+    return_value = ""
+    for structure in napi.structures:
+        return_value =  return_value + "Nest.Home name: " + " " + str(structure.name) + "\n"
+        for ProtectDevice in structure.smoke_co_alarms:
+            return_value = return_value + "Nest.Protect name: " + " " + str(ProtectDevice.name) + " " + str(ProtectDevice.serial) + "\n"
+        for thermostat in structure.thermostats:
+            return_value = return_value + "Nest.Thermostat name: " + " " + str(thermostat.name) + " " + str(thermostat.serial) + "\n"
+        for camera in structure.cameras:
+            return_value = return_value + "Nest.Camera name: " + " " + str(camera.name) + " " + str(camera.serial) + "\n"
+    return return_value
+#    except:
+#	return unicode("ERROR getting Nest information!\nCheck your configuration.\nOr wait some time if you do too much request in little time.", 'utf-8')
 
-def get_device(username , password):
-    napi = nest.Nest(username , password)
-    try:
-	return_value = []
-	for structure in napi.structures:
-            return_value.append({'type':'nest.home','where':str(structure.name),'serial':str(structure.name)})
-            for ProtectDevice in structure.protectdevices:
-		return_value.append({'type':'nest.protect','where':str(ProtectDevice.where),'serial':str(ProtectDevice.serial)})
-            for device in structure.devices:
-		return_value.append({'type':'nest.thermostat','where':str(device.where),'serial':str(device.name)})
-        return return_value
-    except:
-	return unicode("ERROR getting Nest information\nCheck your configuration", 'utf-8')
+def get_device(product_id , product_secret, access_token_cache_file):
+    napi = nest.Nest(client_id=product_id, client_secret=product_secret, access_token_cache_file=access_token_cache_file)
+#    try:
+    return_value = []
+    for structure in napi.structures:
+        return_value.append({'type':'nest.home','where':str(structure.name),'serial':str(structure.name),'device_name':str(structure.name)})
+        for ProtectDevice in structure.smoke_co_alarms:
+            return_value.append({'type':'nest.protect','where':str(ProtectDevice.where),'serial':str(ProtectDevice.serial)})
+        for thermostat in structure.thermostats:
+            return_value.append({'type':'nest.thermostat','where':str(thermostat.where),'serial':str(thermostat.serial)})
+        for camera in structure.cameras:
+            return_value.append({'type':'nest.camera','where':str(camera.where),'serial':str(camera.serial)})
+    return return_value
+#    except:
+#	return unicode("ERROR getting Nest information\nCheck your configuration", 'utf-8')
+
+class CodeForm(Form):
+    code = StringField("code")
 
 ### common tasks
 package = "plugin_nestdevice"
@@ -62,25 +93,34 @@ template_dir = "{0}/{1}/admin/templates".format(get_packages_directory(), packag
 static_dir = "{0}/{1}/admin/static".format(get_packages_directory(), package)
 geterrorlogcmd = "{0}/{1}/admin/geterrorlog.sh".format(get_packages_directory(), package)
 
+# TODO
+# access_token_cache_file = os.path.join(get_data_files_directory_for_plugin("nestdevice"), nest.json)
+access_token_cache_file = os.path.join(os.path.dirname(__file__), '../data/nest.json')
+
 plugin_nestdevice_adm = Blueprint(package, __name__,
                         template_folder = template_dir,
                         static_folder = static_dir)
 
 
-@plugin_nestdevice_adm.route('/<client_id>')
+@plugin_nestdevice_adm.route('/<client_id>', methods=['GET', 'POST'])
 def index(client_id):
     detail = get_client_detail(client_id)
+    form = CodeForm()
     product_id = str(detail['data']['configuration'][1]['value'])
     product_secret = str(detail['data']['configuration'][2]['value'])
-    access_token_cache_file = 'nest.json'
+
+    if request.method == "POST":
+        generate_token_file(form.code.data, product_id, product_secret, access_token_cache_file)
     try:
         return render_template('plugin_nestdevice.html',
             clientid = client_id,
             client_detail = detail,
             mactive="clients",
             active = 'advanced',
-            device_list = get_device_list(product_id,product_secret,access_token_cache_file),
-            devices = get_device(product_id,product_secret,access_token_cache_file),
+	    get_token_url=get_token_link(product_id, product_secret, access_token_cache_file),
+	    form=form,
+            device_list = get_device_list(product_id, product_secret, access_token_cache_file),
+            devices = get_device(product_id, product_secret, access_token_cache_file),
             errorlog = get_info_from_log(geterrorlogcmd))
 
     except TemplateNotFound:
